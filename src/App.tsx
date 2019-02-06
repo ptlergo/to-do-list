c9ffe68508d05ae18fc543b2ad4d883e87f503c2
ptlergo <ptlergo@gmail.com>
2019-02-06 13:11:46 -0500

    refactor app to pass in actions to displaytask component for state changeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { Form, DisplayTask } from "./components";
 
+/**
+ * the component with the state will have the actions as well that alter the state
+ * pass these actions to child components
+ */
 export class App extends React.Component<{}, IState> {
   constructor(props: {}) {
     super(props);
@@ -17,7 +21,22 @@ export class App extends React.Component<{}, IState> {
     };
   }
 
-  public deleteTask(id: number): void {
+
+  // public method because its affecting html
+  // * i dont think its good to use splice as its mutating the original array
+  // splice changes the contents of an array by replace/remove existing elements and/or adding new ones
+  public toggleDone = (index: number): void => {
+    // remove the given index from array using splice
+    let task: ITask[] = this.state.tasks.splice(index, 1);
+    // first array element in task its 'completed' prop. value is toggled
+    task[0].completed = !task[0].completed;
+    // array of currentTasks is updated
+    const tasks: ITask[] = [...this.state.tasks, ...task];
+    this.setState({ tasks });
+  };
+
+  public deleteTask = (id: number): void => {
+    console.log({ id });
     // filter the task by checking every single one and check if id is not the same
     // then keep going until it is the same and filter it
     const tasks: Array<ITask> = this.state.tasks.filter(
@@ -25,38 +44,7 @@ export class App extends React.Component<{}, IState> {
     );
 
     this.setState({ tasks });
-  }
-
-  // public method because its affecting html
-  // * i dont think its good to use splice as its mutating the original array
-  // splice changes the contents of an array by replace/remove existing elements and/or adding new ones
-  public toggleDone(index: number): void {
-    // remove the given index from array using splice
-    let task: ITask[] = this.state.tasks.splice(index, 1);
-    // first array element in task its 'completed' prop. value is toggled
-    task[0].completed = !task[0].completed;
-    // array of currentTasks is updated
-    const tasks: ITask[] = [...this.state.tasks, ...task];
-
-    this.setState({ tasks });
-  }
-  // delete a task. we need to know which task, which means we need a key
-
-  public renderTasks(): JSX.Element[] {
-    return this.state.tasks.map((task: ITask, index: number) => {
-      return (
-        <div key={task.id} className="tdl-task">
-          <span className={task.completed ? "is-completed" : ""}>
-            {task.value}
-          </span>
-          <button onClick={() => this.deleteTask(task.id)}>Delete</button>
-          <button onClick={() => this.toggleDone(index)}>
-            {task.completed ? "undo" : "done"}
-          </button>
-        </div>
-      );
-    });
-  }
+  };
 
   public handleChange = (e: any): void => {
     const { value } = e.target;
@@ -86,8 +74,11 @@ export class App extends React.Component<{}, IState> {
           onChange={this.handleChange}
           onSubmit={this.handleSubmit}
         />
-        {this.renderTasks()}
-        {/* <DisplayTask tasks={this.state.tasks} /> */}
+        <DisplayTask
+          tasks={this.state.tasks}
+          toggleDone={this.toggleDone}
+          deleteTask={this.deleteTask}
+        />
       </div>
     );
   }
--- src/components/DisplayTask.tsx
+++ src/components/DisplayTask.tsx
@@ -1,9 +1,45 @@
 import * as React from "react";
 
 export default (props: any) => {
-    const {tasks} = props;
-    const tot = tasks != 0;
-  return <h1>
-      {tasks}
-  </h1>;
+  const { tasks, toggleDone, deleteTask } = props;
+
+  const renderTaskHtml = (task: any) => {
+    return (
+      <span className={task.completed ? "is-completed" : ""}>{task.value}</span>
+    );
+  };
+
+  // this should be in parent container as it alter state
+  /**
+   * @todo how to handle state changes in child component: react hooks will work well 
+   */
+  const renderTaskButtonsHtml = (task: any, index: number) => {
+    return (
+      <span>
+        <button onClick={() => props.deleteTask(task.id)}>Delete</button>
+        <button onClick={() => props.toggleDone(index)}>
+          {task.completed ? "undo" : "done"}
+        </button>
+      </span>
+    );
+  };
+
+  const renderHtml = (): JSX.Element[] => {
+    return tasks.map((task: ITask, index: number) => {
+      return (
+        <article key={task.id} className="tdl-task">
+          {renderTaskHtml(task)}
+          {renderTaskButtonsHtml(task, index)}
+        </article>
+      );
+    });
+  };
+
+  return <section>{renderHtml()}</section>;
 };
+
+interface ITask {
+  id: number;
+  value: string;
+  completed: boolean;
+}
